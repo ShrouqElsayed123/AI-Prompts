d8986e43ce1983cbc7656f57d574ebd1198d98b0
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-05-25 09:56:12 -0700

    create components

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Feed from '@components/Feed'
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-red-950 border-2 border-rose-600">Test</h1>
+
+      <Feed />
+    </div>
+
+  )
+}
+
+export default Home 
